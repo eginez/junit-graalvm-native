9ba1e8797ec5d7eba79a351de88f241d84585f76
Esteban Ginez <esteban.ginez@oracle.com>
2020-05-08 17:26:54 -0700

    Some tests with Junit 5

--- src/main/java/xyz/eginez/junit/NativeImageExtension.java
+++ src/main/java/xyz/eginez/junit/NativeImageExtension.java
@@ -1,66 +1,29 @@
 package xyz.eginez.junit;
 
-import com.oracle.svm.core.c.function.CEntryPointOptions;
-import com.oracle.svm.hosted.ImageClassLoader;
-import com.oracle.svm.hosted.NativeImageClassLoader;
-import com.oracle.svm.hosted.NativeImageGenerator;
-import com.oracle.svm.hosted.NativeImageGeneratorRunner;
-import com.oracle.svm.hosted.NativeImageOptions;
-import com.oracle.svm.hosted.analysis.Inflation;
-import com.oracle.svm.hosted.code.CEntryPointData;
-import com.oracle.svm.hosted.image.AbstractBootImage;
-import com.oracle.svm.hosted.option.HostedOptionCustomizer;
-import com.oracle.svm.hosted.option.HostedOptionParser;
-import org.graalvm.collections.Pair;
-import org.graalvm.compiler.debug.DebugContext;
-import org.graalvm.compiler.debug.DebugHandlersFactory;
-import org.graalvm.compiler.options.OptionValues;
-import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.concurrent.ForkJoinPool;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class NativeImageExtension {
-    public static void compileMethod(Method method) {
-        final String[] classpath = System.getProperty("java.class.path").split(File.pathSeparator);
-        System.out.println("Creating class loader with" + String.join("\n ", Arrays.asList(classpath)));
-        NativeImageClassLoader classLoader = NativeImageGeneratorRunner.installNativeImageClassLoader(classpath);
-        ImageClassLoader imageClassLoader = ImageClassLoader.create(NativeImageGenerator.defaultPlatform(classLoader), classpath, classLoader);
+public class NativeImageExtension implements TestWatcher, AfterAllCallback {
+    Set<Class<?>> testClasses = new HashSet<>();
 
-        HostedOptionParser optionParser = new HostedOptionParser(imageClassLoader);
-        final HostedOptionCustomizer customizer = new HostedOptionCustomizer(optionParser);
-
-        AbstractBootImage.NativeImageKind kind = AbstractBootImage.NativeImageKind.SHARED_LIBRARY;
-
-        final CEntryPointData cEntryPointData = CEntryPointData.create(method, method.getName(), CEntryPointData.DEFAULT_NAME_TRANSFORMATION,
-                "", CEntryPointOptions.NoPrologue.class, CEntryPointOptions.NoEpilogue.class,
-                CEntryPoint.FatalExceptionHandler.class, CEntryPointOptions.Publish.SymbolOnly);
-        final Pair<Method, CEntryPointData> entryPoint = Pair.create(method, cEntryPointData);
-
-        final OptionValues parsedHostedOptions = new OptionValues(optionParser.getHostedValues());
-        DebugContext debug = DebugContext.create(parsedHostedOptions, DebugHandlersFactory.LOADER);
-        //DebugContext debug = new DebugContext.Builder(parsedHostedOptions).build();
-        ForkJoinPool analysisExecutor = Inflation.createExecutor(debug, NativeImageOptions.getMaximumNumberOfAnalysisThreads(parsedHostedOptions));
-        ForkJoinPool compilationExecutor = Inflation.createExecutor(debug, NativeImageOptions.getMaximumNumberOfConcurrentThreads(parsedHostedOptions));
-
-        final NativeImageGenerator nativeImageGenerator = new NativeImageGenerator(imageClassLoader, customizer, entryPoint);
-        System.out.println("Init image build");
-        final HashMap<Method, CEntryPointData> entryPData = new HashMap<>();
-        entryPData.put(method, cEntryPointData);
-        nativeImageGenerator.run(entryPData, null, method.getName(), kind, null, compilationExecutor, analysisExecutor, optionParser.getRuntimeOptionNames());
-
-    }
-
-    public void add() {
-        System.out.println("1 + 1 == 2");
+    @Override
+    public void testSuccessful(ExtensionContext context) {
+        testClasses.add(context.getRequiredTestClass());
     }
 
 
-    public static void main(String[] args) throws Exception {
-        final Method add = NativeImageExtension.class.getDeclaredMethod("add");
-        compileMethod(add);
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        final String all = testClasses.stream().map(Class::getName).collect(Collectors.joining(","));
+        System.out.println(all);
+
+
     }
 }
--- src/test/java/xyz/eginez/junit/TestSimple.java
+++ src/test/java/xyz/eginez/junit/TestSimple.java
@@ -1,12 +1,18 @@
 package xyz.eginez.junit;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-//@ExtendWith(NativeImageExtension.class)
-public class TestSimple {
+@ExtendWith(NativeImageExtension.class)
+class TestSimple {
     @Test
-    public  void one() {
-        System.out.println("hello");
+    void one() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void two() {
+        Assertions.assertEquals(2, 1 + 1);
     }
 }
